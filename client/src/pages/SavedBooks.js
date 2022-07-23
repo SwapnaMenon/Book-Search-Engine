5423b5d10a73b023f7fa2d764fd990be6d7086dd
Swapna <swapnamenon8@gmail.com>
2022-07-22 21:29:05 -0400

    syntax change in the savedbooks.js

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,5 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
+import { Jumbotron, 
+  Container, 
+  CardColumns, 
+  Card, 
+  Button 
+} from 'react-bootstrap';
 import { useQuery, useMutation } from "@apollo/client";
 import { REMOVE_BOOK } from "../utils/mutations";
 import { getMe, deleteBook } from '../utils/API';
