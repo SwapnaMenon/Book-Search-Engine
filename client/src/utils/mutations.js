9ef7dd2ff88d2adbbb72ad2aa6c2aa94e3b484ef
Swapna <swapnamenon8@gmail.com>
2022-07-24 17:26:12 -0400

    changes

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,7 +1,7 @@
 import { gql } from '@apollo/client';
 
 export const LOGIN_USER = gql`
-  mutation login($email: String!, $password: String!) {
+  mutation login($email: String, $password: String) {
     login(email: $email, password: $password) {
       token
       user {
@@ -13,7 +13,7 @@ export const LOGIN_USER = gql`
 `;
 
 export const ADD_USER = gql`
-  mutation addUser($username: String!, $email: String!, $password: String!) {
+  mutation addUser($username: String, $email: String, $password: String) {
     addUser(username: $username, email: $email, password: $password) {
       token
       user {
@@ -25,8 +25,8 @@ export const ADD_USER = gql`
 `;
 
 export const SAVE_BOOK = gql`
-  mutation saveBook($bookData: BookInput!) {
-    saveBook(bookData: $bookData) {
+  mutation saveBook($book: saveBooks) {
+    saveBook(book: $book) {
       _id
       username
       email
